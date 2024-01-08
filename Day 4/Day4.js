d0af46d34a23d157eba72f7ef93a84d88afa749d
Abhinav Gupta <abhinavgupta1702@gmail.com>
2024-01-08 21:11:54 +0530

    Create Day4.js

---
+++ Day 4/Day4.js
@@ -0,0 +1,16 @@
+// loops are used to repeat certain blocks of code
+
+// for loop is used to repeat a block of code for a finite time
+
+console.log("Using For Loop");
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// while loop is used to repeat a block of code until the condition specified becomes false
+let j = 1;
+console.log("Using While Loop");
+while (j <= 10){
+    console.log(j);
+    j++;
+}
