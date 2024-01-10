13fcccfc7a8443ae164c6a461d0788aad29c9f82
Abhinav Gupta <abhinavgupta1702@gmail.com>
2024-01-10 12:37:57 +0530

    Create day6.js

---
+++ Day 6/day6.js
@@ -0,0 +1,10 @@
+// use the function keyword to define functions
+
+function calculateSum(a, b){
+    let result = a + b;
+    return console.log(`Result: ${result}`); // return keyword is used to return a value evaluated in the function
+}
+
+// calling the function with the parameters
+calculateSum(2, 4);
+calculateSum(3, 5);
