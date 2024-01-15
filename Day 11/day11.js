ff11250f161abd4358a61fa2e300a2d3de412ad6
Abhinav Gupta <abhinavgupta1702@gmail.com>
2024-01-15 19:53:31 +0530

    Update day11.js

--- Day 11/day11.js
+++ Day 11/day11.js
@@ -4,11 +4,11 @@ let result = document.getElementById("result");
 function validateEmail(){
     let emailId = prompt("Enter your Email Address.")
     let check = regex.test(emailId);
-    
+
     if (check){
         result.textContent = emailId + " is a valid Email Address."
     }
     else{
         result.textContent = emailId + " is not a valid Email Address."
     }
-}  
+}
