569f12b92497d7e0be552badbcb285a7727b168c
Abhinav Gupta <abhinavgupta1702@gmail.com>
2024-01-17 19:13:22 +0530

    Update day11.js

--- Day 11/day11.js
+++ Day 11/day11.js
@@ -2,13 +2,13 @@ const regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
 let result = document.getElementById("result");
 
 function validateEmail(){
-    let emailId = prompt("Enter your Email Address.")
+    let emailId = prompt("Enter your Email Address.");
     let check = regex.test(emailId);
 
     if (check){
-        result.textContent = emailId + " is a valid Email Address."
+        result.textContent = emailId + " is a valid Email Address.";
     }
     else{
-        result.textContent = emailId + " is not a valid Email Address."
+        result.textContent = emailId + " is not a valid Email Address.";
     }
 }
