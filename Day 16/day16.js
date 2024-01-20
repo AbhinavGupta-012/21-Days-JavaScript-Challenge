6da5a75bb37c625a7db4968bbd181dd8e981f914
Abhinav Gupta <abhinavgupta1702@gmail.com>
2024-01-20 13:48:23 +0530

    Create day16.js

---
+++ Day 16/day16.js
@@ -0,0 +1,25 @@
+let delayTime = 3000;
+let millisecondsToSeconds = delayTime / 1000;
+let day = prompt("Which day of the challenge is today?")
+
+let simulateAsync = () => {
+    return new Promise((myResolve, myReject) => {
+         setTimeout(() =>{
+                if (day == 16){
+                    myResolve(`Today is Day ${day}.`);
+                }
+                else{
+                    myReject(`Today is not Day ${day}!`);
+                }
+            }, delayTime);
+        }
+    );
+}
+
+simulateAsync()
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
